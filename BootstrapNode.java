15ae810a9882e41e3df9fdb894943ef898dafb3a
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2013-11-19 01:33:59 -0800

    committing hideous but marginally functional code
    the code will register nodes to the registry reliably, but the RMINode
    logic is a bit undertested, so it throws a StackOverflowException every
    time.  Too tired to fix it tonight

--- BootstrapNode.java
+++ BootstrapNode.java
@@ -1,9 +1,9 @@
+import java.io.IOException;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.rmi.AlreadyBoundException;
-import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -23,21 +23,34 @@ public class BootstrapNode {
 	 *            String arguments; expects the name of the node and the port of
 	 *            the registry, respectively.
 	 * 
-	 * @throws MalformedURLException
-	 * @throws RemoteException
 	 * @throws AlreadyBoundException 
-	 * @throws UnknownHostException 
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) throws MalformedURLException, RemoteException, AlreadyBoundException, UnknownHostException {
-		if (args.length != 2) {
-			Log.err("Usage: BootstrapNode <registryAddress> <registryPort>");
+	public static void main(String[] args) throws AlreadyBoundException, IOException {
+		if (args.length != 3) {
+			Log.err("Usage: BootstrapNode <registryAddress> <registryPort> <m>");
 		} else {
 			String registryHostname = args[0];
 			int registryPort = Integer.parseInt(args[1]);
+			int m = Integer.parseInt(args[2]);
 
-			RMINode node = new RMINode(4, InetAddress.getLocalHost());
 			Registry fakeDNS = LocateRegistry.getRegistry(registryHostname, registryPort);
+			RMINode node = new RMINode(m, generateInetSocketAddress());
 			fakeDNS.rebind("" + node.getNodeKey(), UnicastRemoteObject.exportObject(node, 0));
+			
+			Log.out("seeded new chord network with node id " + node.getNodeKey());
 		}
 	}
+	
+	private static InetSocketAddress generateInetSocketAddress() throws IOException{
+		InetAddress localhost = InetAddress.getLocalHost();
+		int port;
+		try (ServerSocket incrediblyInefficientMeansOfAcquiringAPortNumber = new ServerSocket(0)){
+			port = incrediblyInefficientMeansOfAcquiringAPortNumber.getLocalPort();
+			incrediblyInefficientMeansOfAcquiringAPortNumber.close();
+		}
+		Log.out("port: " + port);
+		return new InetSocketAddress(localhost, port);
+	}
+
 }
