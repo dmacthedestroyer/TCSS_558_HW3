34f0240ca2527c0e7af0b00952258ae9147492a6
Jesse Carrigan <jesse.m.carrigan@gmail.com>
2013-11-18 21:04:00 -0800

    Removed unused import from RMIController. Added executive summary.

--- RMIController.java
+++ RMIController.java
@@ -1,6 +1,5 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.rmi.server.UnicastRemoteObject;
 
 /**
  * The RMIController provides the RMI registry service for the Chord network.
