c3ceb52b91b106400d7fbf44b6087e88da1b8329
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2013-11-20 21:34:42 -0800

    remove crusty ol' RemoteLogger

--- JoinNode.java
+++ JoinNode.java
@@ -1,32 +1,31 @@
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
 import java.rmi.NotBoundException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-
 public class JoinNode {
 
-	public static void main(String[] args) throws IOException, NotBoundException {
+	public static void main(String[] args) throws IOException,
+			NotBoundException {
 		if (args.length != 4) {
 			Log.err("Usage: JoinNode <registryAddress> <registryPort> <nodeKeyFromNetwork> <newNodeKey>");
 			return;
 		}
-		
+
 		String registryHostname = args[0];
 		int registryPort = Integer.parseInt(args[1]);
 		String nodeKey = args[2];
 		long newNodeKey = Long.parseLong(args[3]);
 
-		Registry fakeDNS = LocateRegistry.getRegistry(registryHostname, registryPort);
-		RMINodeServer fromNetwork = (RMINodeServer)fakeDNS.lookup(nodeKey);
-		
+		Registry fakeDNS = LocateRegistry.getRegistry(registryHostname,
+				registryPort);
+		RMINodeServer fromNetwork = (RMINodeServer) fakeDNS.lookup(nodeKey);
+
 		RMINode node = new RMINode(fromNetwork.getHashLength(), newNodeKey);
-		fakeDNS.rebind("" + node.getNodeKey(), UnicastRemoteObject.exportObject(node, 0));
-		
+		fakeDNS.rebind("" + node.getNodeKey(),
+				UnicastRemoteObject.exportObject(node, 0));
+
 		node.join(fromNetwork);
 		Log.out("Bound new node to id " + node.getNodeKey());
 	}
--- Logger.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-
-public interface Logger extends Remote {
-	public void log(String log) throws RemoteException;
-}
